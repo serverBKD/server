268239b5d9cfb9b96097fa1151b3701fff7e3d7c
serverBKD <carlosm1bravoa@gmail.com>
2025-05-27 15:16:39 -0400

    2025MAY.27: v0.0.4: dotenv

--- src/index.js
+++ src/index.js
@@ -1,19 +1,25 @@
-import express from 'express'
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(express.json())
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-}
-)
-app.post('/data', (req, res) => {
-    const data = req.body
-    res.json({
-        message: 'Data received successfully',
-        data: data
-    })
-})
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/data", (req, res) => {
+  const data = req.body;
+  res.json({
+    message: "Data received successfully",
+    data: data,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
